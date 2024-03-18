bd190395425002c7173453b3cb4580f84478b17f
Mathavan208 <210701154@rajalakshmi.edu.in>
2024-03-18 21:05:49 +0530

    job finder

--- server/database.js
+++ server/database.js
@@ -5,5 +5,11 @@ password:{type:String,minlength:12,maxlength:120,required:true},
 token:{type:String,required:true},
 date: { type: Date, default: Date.now }
 });
+const teacher=mongoose.Schema({
+	staffId:{type:String,minlength:5,required:true,unique:true},
+	staffname:{type:String,required:true}
+});
+const Teachers=mongoose.model('teachers',teacher);
 const Users=mongoose.model('users',user);
+exports.Teachers=Teachers
 exports.Users=Users;
--- server/routes/excel.js
+++ server/routes/excel.js
@@ -1,27 +1,38 @@
 const express = require('express');
-const ExcelJS = require('exceljs');
+const XLSX = require('xlsx');
 const routes = express.Router();
-
+const {Teachers}=require('../database');
+ const teacher=[];
+ let data1=[];
+ // fetches a whole data and stores it in a array as a json objects
+ // then fetches a data in particular column 
 routes.get('/', async (req, res) => {
-    try {
-        const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.readFile('./file.xlsx');
-        const worksheet = workbook.getWorksheet(1);
-        
-        var data=[];
-        worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
-    const rowData = [];
-    row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
-        rowData.push(cell.value);
-    });
-    data.push(rowData);
+   const workbook=await XLSX.readFile('file.xlsx');
+    const sheet=workbook.SheetNames[0];
+    const worksheet=workbook.Sheets[sheet];
+    const data=XLSX.utils.sheet_to_json(worksheet);
+    data1=[...data];
+    console.log(data);
+    
+const colname='A';
+for(let i=2; ;i++){
+    const celladdress=`${colname}${i}`;
+    const cell=worksheet[celladdress];
+    //console.log(cell);
+    if(!cell||!cell.v)break;
+    teacher.push(cell.v);
+}
+console.log(teacher);
 });
+// it generally uploads the data into a database
+routes.get('/upload',async (req,res)=>{
+ for(const staff of data1){
+    const teacher=await new Teachers({
+        staffId:staff['STAFF ID'],
+        staffname:staff['STAFF NAME']
+    })
+    const res=await teacher.save();
+ }
 
-        res.send(data);
-    } catch (error) {
-        console.error('Error reading the Excel file:', error);
-        res.status(500).send('Error reading the Excel file');
-    }
 });
-
 module.exports = routes;
